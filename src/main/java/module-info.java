b632099727f70e14d500ab5dd5e6adcde99874e6
Raza <141234596+KMORaza@users.noreply.github.com>
2025-05-08 23:53:12 +0530

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module powertrain.ev.simulation.evpowertrainsimulation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens powertrain.ev.simulation to javafx.fxml;
-    exports powertrain.ev.simulation;
-}
